c8d6bf89b0885e155fc71361bfe872406557f2a7
Mangal-Kushwah <63869813+Link2324@users.noreply.github.com>
2024-03-08 16:21:39 +0530

    Add operate function

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ function multipy(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+let fistNumber;
+let secondNumber;
+let operator;   
+let label = "";
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '+': return add(num1, num2);
+
+        case '-': return subtract(num1, num2);
+
+        case "x": return multipy(num1, num2);
+
+        case "/": return divide(num1, num2);
+    }
+}
